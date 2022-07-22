5f0530afdb23fcfa4440b5765a49d59b0a7bcee6
Adriana <a.s.m27267@gmail.com>
2022-07-21 21:06:17 -0300

    Merge js

--- script.js
+++ script.js
@@ -2,22 +2,15 @@ const addPost=() =>{
     fetch('https://jsonplaceholder.typicode.com/posts',{ 
     method:'POST',
     body: JSON.stringify({
-<<<<<<< HEAD
         userId: document.querySelector('#formid'),
         title : document.querySelector('#titulo1'),
         body: document.querySelector('#name'),
-=======
-        userId: 3,
-        title : 'Cadastro de Usuário',
-        body: 'nome'
->>>>>>> dc32165e859e0df5acd0c179f563504d7d0d8d2c
     }),
     headers:{
         'Content-type': 'application/json; charset =UTF-8',
     },
     
     })
-<<<<<<< HEAD
     .then(responce =>{
         responce.json()
         .then(data => console.log(data))
@@ -25,10 +18,4 @@ const addPost=() =>{
     })
 }
 
-=======
-    .then(responce => responce.json())
-    .then(data => console.log(data))
-
-}
->>>>>>> dc32165e859e0df5acd0c179f563504d7d0d8d2c
 addPost();
