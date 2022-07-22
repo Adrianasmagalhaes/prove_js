bf76dcfe3436c6d9f4301f9c5f36738117332b83
Adriana <a.s.m27267@gmail.com>
2022-07-21 21:02:02 -0300

    prova atualizada

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const addPost=() =>{
     fetch('https://jsonplaceholder.typicode.com/posts',{ 
     method:'POST',
     body: JSON.stringify({
-        userId: 3,
-        title : 'Cadastro de Usuário',
-        body: 'nome'
+        userId: document.querySelector('#formid'),
+        title : document.querySelector('#titulo1'),
+        body: document.querySelector('#name'),
     }),
     headers:{
         'Content-type': 'application/json; charset =UTF-8',
     },
     
     })
-    .then(responce => responce.json())
-    .then(data => console.log(data))
-
+    .then(responce =>{
+        responce.json()
+        .then(data => console.log(data))
+    
+    })
 }
+
 addPost();
