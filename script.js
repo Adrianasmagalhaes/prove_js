edc019d7ddaf04f6d6c81a27ffbf6f062ff01ff7
Adriana <a.s.m27267@gmail.com>
2022-07-21 22:53:17 -0300

    post

--- script.js
+++ script.js
@@ -2,17 +2,18 @@ const addPost=() =>{
     fetch('https://jsonplaceholder.typicode.com/posts',{ 
     method:'POST',
     body: JSON.stringify({
-        userId: 3,
-        title : 'Cadastro de Usuário',
-        body: 'nome'
+        title: document.querySelector('#name'),
+        body: document.querySelector('#email'),
+        userId: document.querySelector('#userName'),
     }),
-    headers:{
-        'Content-type': 'application/json; charset =UTF-8',
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
     },
-    
-    })
-    .then(responce => responce.json())
-    .then(data => console.log(data))
+})
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json)
 
+        createUser(json.id)
+    });
 }
-addPost();
